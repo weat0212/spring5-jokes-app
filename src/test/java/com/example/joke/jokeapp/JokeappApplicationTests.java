cb04af542ad4f16e7f4d29a9c1c789c29a5dde9a
weat0212 <weat0212@gmail.com>
2020-11-19 15:32:53 +0800

    first commit

---
+++ src/test/java/com/example/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
